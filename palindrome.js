63c72d2eb115c137f0cade878ab56672280e75b1
Laury <laurynejosiasdadygmail.com>
2023-07-18 19:14:55 +0100

    review

--- palindrome.js
+++ palindrome.js
@@ -4,13 +4,14 @@ function isPalindrome(char) {
    
     let debut = 0;
     let fin = char.length - 1;
+    
 
     while (debut < fin) {
-        if (!char[debut].match(/[a-z]/i)) {
+        if (!char[debut].match(/[a-z0-9]/i)) {
             debut++;
             continue;
         }
-        if (!char[fin].match(/[a-z]/i)) {
+        if (!char[fin].match(/[a-z0-9]/i)) {
             fin--;
             continue;
         }
@@ -24,3 +25,17 @@ function isPalindrome(char) {
     return true;
 }
 console.log(isPalindrome('My age is 0, 0 si ega ym'))
+
+
+console.log(isPalindrome("eye")) // should return true.
+console.log(isPalindrome("_eye")) // should return true.
+console.log(isPalindrome("race car")) // should return true.
+console.log(isPalindrome("not a isPalindrome")) // should return false.
+console.log(isPalindrome("A man, a plan, a canal. Panama") )// should return true.
+console.log(isPalindrome("never odd or even")) // should return true.
+console.log(isPalindrome("nope")) // should return false.
+console.log(isPalindrome("almostomla")) // should return false.
+console.log(isPalindrome("My age is 0, 0 si ega ym.")) // should return true.
+console.log(isPalindrome("1 eye for of 1 eye.") )// should return false.
+console.log(isPalindrome("0_0 (: /-\ :) 0-0")) // should return true.
+console.log(isPalindrome("five|\_/|four")) // should return false.
